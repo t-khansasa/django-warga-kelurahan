065cf94d198f9149d6d574b29528bb6a5a587e34
t-khansasa <khansatahirasasa254254@gmail.com>
2025-10-07 21:02:31 +0700

    update

--- warga/admin.py
+++ warga/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Warga
+from .models import Warga, Pengaduan
 
 admin.site.register(Warga)
+admin.site.register(Pengaduan)
 
 # Register your models here.
