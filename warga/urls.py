6ef35e41b3dc39c1dcf66a3db75837fe655fab9c
t-khansasa <khansatahirasasa254254@gmail.com>
2025-10-08 11:53:15 +0700

    update 2

--- warga/urls.py
+++ warga/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import WargaListView, WargaDetailView
+from .views import WargaListView, WargaDetailView, PengaduanListView
 
 urlpatterns = [
     path('', WargaListView.as_view(), name='warga-list'),
     path('<int:pk>/', WargaDetailView.as_view(), name='warga-detail'),
+    path('pengaduan/', PengaduanListView.as_view(), name='pengaduan-list'),
 ]
 
 
--- warga/views.py
+++ warga/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView
-from .models import Warga
+from .models import Warga, Pengaduan
 
 class WargaListView(ListView):
     model = Warga
@@ -9,4 +9,10 @@ class WargaListView(ListView):
 class WargaDetailView(DetailView):
     model = Warga
     template_name = 'warga/warga_detail.html'
+
+class PengaduanListView(ListView):
+    model = Pengaduan
+    template_name = 'warga/pengaduan_list.html'  
+    context_object_name = 'object_list' 
+
 # Create your views here.
